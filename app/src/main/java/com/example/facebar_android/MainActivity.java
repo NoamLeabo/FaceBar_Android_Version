576ba45c828a3afa148a18d4035f0a61294a517c
NoamLeabo <noam.leabovich@gmail.com>
2024-02-10 21:14:56 +0200

    first basic layout

--- app/src/main/java/com/example/facebar_android/MainActivity.java
+++ app/src/main/java/com/example/facebar_android/MainActivity.java
@@ -1,14 +1,28 @@
 package com.example.facebar_android;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.feed_page);
+
+        Button settingsBtn = findViewById(R.id.settings_Btn);
+        settingsBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Change button background color dynamically
+                settingsBtn.setBackgroundResource(R.drawable.button_background);
+            }
+        });
     }
+
+
+
 }
