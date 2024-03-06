b7917df5d498912250af20cc6988a89416039b0f
gideonn12 <gideoneeman@icloud.com>
2024-03-06 21:46:53 +0200

    starting connect user

---
+++ app/src/main/java/com/example/facebar_android/UserAPI.java
@@ -0,0 +1,32 @@
+package com.example.facebar_android;
+
+import retrofit2.Call;
+import retrofit2.http.Body;
+import retrofit2.http.DELETE;
+import retrofit2.http.GET;
+import retrofit2.http.PATCH;
+import retrofit2.http.POST;
+import retrofit2.http.Path;
+
+public interface UserAPI {
+    @GET("user/{id}")
+    Call<Void> getUser(@Path("id") int id);
+
+    @PATCH("user/{id}")
+    Call<Void> updateUser(@Path("id") int id);
+
+    @DELETE("user/{id}")
+    Call<Void> deleteUser(@Path("id") int id);
+
+    @GET("user/{id}/friends")
+    Call<Void> getFriends(@Path("id") int id);
+
+    @POST("user/{id}/friends")
+    Call<Void> pendingFriend(@Path("id") int id, @Body int fid);
+
+    @PATCH("user/{id}/friends/{fid}")
+    Call<Void> acceptFriend(@Path("id") int id);
+
+    @DELETE("user/{id}/friends/{fid}")
+    Call<Void> rejectFriend(@Path("id") int id);
+}
